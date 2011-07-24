b8ecf3fe1b8210dfc8b226643e09f0e27089c92c
trebor_7 <trebor_7@users.sourceforge.net>
2011-07-24 13:44:29 +0200

    Fixed fueldump.bat ...

--- maps/fueldump.bat
+++ maps/fueldump.bat
@@ -1,3 +1,3 @@
-"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -meta battery.map
-"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -vis -fast battery.map
-"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -light -fast -filter -samplesize 1 -lightmapsize 2048 -shadeangle 179 battery.map
+"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -meta fueldump.map
+"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -vis -fast fueldump.map
+"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -light -fast -filter -samplesize 1 -lightmapsize 2048 -shadeangle 179 fueldump.map
