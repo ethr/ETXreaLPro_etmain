08f93cd14aad34364933405a68b088d7b29e7311
trebor_7 <trebor_7@users.sourceforge.net>
2011-08-07 13:52:57 +0200

    Removed old .map files and added etx_ .bat files for compiling the maps in Doom 3 .map format.

---
+++ maps/etx_oasis.bat
@@ -0,0 +1,3 @@
+"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -meta etx_oasis.map
+"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -vis -fast etx_oasis.map
+"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -light -fast -filter -samplesize 1 -lightmapsize 2048 -shadeangle 179 etx_oasis.map
---
+++ maps/etx_radar.bat
@@ -0,0 +1,3 @@
+"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -meta etx_radar.map
+"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -vis -fast etx_radar.map
+"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -light -fast -filter -samplesize 1 -lightmapsize 2048 -shadeangle 179 etx_radar.map
