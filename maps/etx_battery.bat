56e11b37b698a45bdb49344bcd4ee954f27120bf
trebor_7 <trebor_7@users.sourceforge.net>
2011-08-11 14:07:31 +0200

    Fixed leaks in etx_battery.map

--- maps/etx_battery.bat
+++ maps/etx_battery.bat
@@ -1,6 +1,32 @@
-"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -v -meta etx_battery.map
+set VERSION=20110811
+set MAPNAME=etx_battery
+cd ../../bin32
+PATH=%PATH%;%CD%
+cd ..
+set BASEPATH=%CD%
+cd etmain
+
+ETXMap.exe -fs_basepath %BASEPATH% -v -meta -leaktest maps/%MAPNAME%.map
 pause
-"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -vis -fast etx_battery.map
+ETXMap.exe -fs_basepath %BASEPATH% -vis -fast maps/%MAPNAME%.map
 pause
-"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -light -fast -filter -samplesize 1 -lightmapsize 2048 -shadeangle 179 etx_battery.map
-#pause
+ETXMap.exe -fs_basepath %BASEPATH% -light -fast -filter -samplesize 1 -lightmapsize 2048 -shadeangle 179 maps/%MAPNAME%.map
+ETXMap.exe -fs_basepath %BASEPATH% -v -minimap -size 512 -o levelshots/%MAPNAME%_cc2.png maps/%MAPNAME%
+
+del map-%MAPNAME%-%VERSION%.pk3
+zip map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%.png
+zip map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%_cc.png
+zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bat
+zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.map
+zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bsp
+zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.objdata
+zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.script
+zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_lms.objdata
+zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_lms.script
+zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_tracemap.tga
+zip -r map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%/
+pause
+
+
+
+
