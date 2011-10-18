1997bef5c5694ffc3d12681d4f6047fe5ba4439e
trebor_7 <trebor_7@users.sourceforge.net>
2011-10-18 21:24:09 +0200

    Updated map compile scripts.

--- maps/etx_battery.bat
+++ maps/etx_battery.bat
@@ -1,31 +1,31 @@
-set VERSION=20111015
+set VERSION=20111018
 set MAPNAME=etx_battery
 cd ../../bin/win64
 PATH=%PATH%;%CD%
-cd ..
+cd ../..
 set BASEPATH=%CD%
 cd etmain
 
 ETXMap.exe -fs_basepath %BASEPATH% -v -meta -leaktest maps/%MAPNAME%.map
-pause
+REM pause
 ETXMap.exe -fs_basepath %BASEPATH% -vis -fast maps/%MAPNAME%.map
-pause
-ETXMap.exe -fs_basepath %BASEPATH% -light -fast -filter -samplesize 1 -lightmapsize 2048 -shadeangle 179 maps/%MAPNAME%.map
-ETXMap.exe -fs_basepath %BASEPATH% -v -minimap -size 512 -o levelshots/%MAPNAME%_cc2.png maps/%MAPNAME%
+REM pause
+ETXMap.exe -fs_basepath %BASEPATH% -light -fast -filter -samplesize 1 -lightmapsize -skycale 2.0 -pointscale 1.3 2048 maps/%MAPNAME%.map
+REM ETXMap.exe -fs_basepath %BASEPATH% -v -minimap -size 512 -o levelshots/%MAPNAME%_cc2.png maps/%MAPNAME%
 
-del map-%MAPNAME%-%VERSION%.pk3
-zip map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%.png
-zip map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%_cc.png
-zip map-%MAPNAME%-%VERSION%.pk3 scripts/%MAPNAME%.arena
-zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bat
-zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.map
-zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bsp
-zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.objdata
-zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.script
-zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_lms.objdata
-zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_lms.script
-zip map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_tracemap.tga
-zip -r map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%/
+del xreal-map-%MAPNAME%-%VERSION%.pk3
+REM zip xreal-map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%.png
+REM zip xreal-map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%_cc.png
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 scripts/%MAPNAME%.arena
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bat
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.map
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bsp
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.objdata
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.script
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_lms.objdata
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_lms.script
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_tracemap.tga
+zip -r xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%/
 pause
 
 
--- maps/etx_radar.bat
+++ maps/etx_radar.bat
@@ -1,6 +1,26 @@
-"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -v -meta -leaktest etx_radar.map
-#pause
-"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -vis etx_radar.map
-#pause
-"../../bin32/ETXMap.exe" -fs_basepath %CD%/../.. -light -fast -filter -samplesize 1 -lightmapsize 2048 -shadeangle 179 etx_radar.map
-#pause
+set VERSION=20111018
+set MAPNAME=etx_radar
+cd ../../bin/win64
+PATH=%PATH%;%CD%
+cd ../..
+set BASEPATH=%CD%
+cd etmain
+
+ETXMap.exe -fs_basepath %BASEPATH% -v -meta -leaktest maps/%MAPNAME%.map
+ETXMap.exe -fs_basepath %BASEPATH% -vis -fast maps/%MAPNAME%.map
+ETXMap.exe -fs_basepath %BASEPATH% -light -fast -filter -samplesize 1 -lightmapsize 2048 maps/%MAPNAME%.map
+REM ETXMap -v -minimap -size 512 -o levelshots/%MAPNAME%_cc2.png maps/%MAPNAME%
+
+del xreal-map-%MAPNAME%-%VERSION%.pk3
+REM zip xreal-map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%.png
+REM zip xreal-map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%_cc.png
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bat
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.map
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bsp
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.objdata
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.script
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_lms.objdata
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_lms.script
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%_tracemap.tga
+zip -r xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%/
+pause
