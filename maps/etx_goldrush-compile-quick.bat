b3895e271c69075b72a18df5e0af66ea54f00060
trebor_7 <trebor_7@users.sourceforge.net>
2011-10-23 15:23:59 +0200

    Converted original ET .map files into Doom 3 .map versions etx_*.map

--- maps/etx_goldrush-compile-quick.bat
+++ maps/etx_goldrush-compile-quick.bat
@@ -7,6 +7,7 @@ set BASEPATH=%CD%
 cd etmain
 
 ETXMap.exe -fs_basepath %BASEPATH% -v -meta -leaktest maps/%MAPNAME%.map
+pause
 ETXMap.exe -fs_basepath %BASEPATH% -vis -fast maps/%MAPNAME%.map
 ETXMap.exe -fs_basepath %BASEPATH% -light -fast -samplesize 16 -lightmapsize 2048 maps/%MAPNAME%.map
 REM ETXMap -v -minimap -size 512 -o levelshots/%MAPNAME%_cc.png maps/%MAPNAME%
