3bb65d59efa8ef5aaee89e8ae339d426ed81f603
trebor_7 <trebor_7@users.sourceforge.net>
2011-10-23 20:53:57 +0200

    Tweaked map compile scripts.

--- maps/etx_radar-compile-final.bat
+++ maps/etx_radar-compile-final.bat
@@ -1,20 +1,21 @@
-set VERSION=20111018
-set MAPNAME=etx_railgun
-cd ../../bin/win64
+set VERSION=20111023
+set MAPNAME=etx_radar
+cd ../../bin/win32
 PATH=%PATH%;%CD%
 cd ../..
 set BASEPATH=%CD%
 cd etmain
 
 ETXMap.exe -fs_basepath %BASEPATH% -v -meta -leaktest maps/%MAPNAME%.map
-ETXMap.exe -fs_basepath %BASEPATH% -vis -fast maps/%MAPNAME%.map
+ETXMap.exe -fs_basepath %BASEPATH% -vis maps/%MAPNAME%.map
 ETXMap.exe -fs_basepath %BASEPATH% -light -fast -filter -samplesize 1 -lightmapsize 2048 maps/%MAPNAME%.map
 REM ETXMap -v -minimap -size 512 -o levelshots/%MAPNAME%_cc2.png maps/%MAPNAME%
 
 del xreal-map-%MAPNAME%-%VERSION%.pk3
 REM zip xreal-map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%.png
 REM zip xreal-map-%MAPNAME%-%VERSION%.pk3 levelshots/%MAPNAME%_cc.png
-zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bat
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%-compile-final.bat
+zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%-compile-quick.bat
 zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.map
 zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.bsp
 zip xreal-map-%MAPNAME%-%VERSION%.pk3 maps/%MAPNAME%.objdata
--- maps/etx_radar-compile-quick.bat
+++ maps/etx_radar-compile-quick.bat
@@ -1,5 +1,5 @@
 set VERSION=20111023
-set MAPNAME=etx_goldrush
+set MAPNAME=etx_radar
 cd ../../bin/win32
 PATH=%PATH%;%CD%
 cd ../..
@@ -7,9 +7,8 @@ set BASEPATH=%CD%
 cd etmain
 
 ETXMap.exe -fs_basepath %BASEPATH% -v -meta -leaktest maps/%MAPNAME%.map
-pause
 ETXMap.exe -fs_basepath %BASEPATH% -vis -fast maps/%MAPNAME%.map
 ETXMap.exe -fs_basepath %BASEPATH% -light -fast -samplesize 16 -lightmapsize 2048 maps/%MAPNAME%.map
-REM ETXMap -v -minimap -size 512 -o levelshots/%MAPNAME%_cc.png maps/%MAPNAME%
-pause
+REM ETXMap -v -minimap -size 512 -o levelshots/%MAPNAME%_cc2.png maps/%MAPNAME%
 
+pause
