dcc6600d60ce5c8734e08b361242e83d99f6ba39
trebor_7 <trebor_7@users.sourceforge.net>
2011-10-26 01:50:07 +0200

    Improved deluxe mapping.

--- maps/etx_battery-compile-final.bat
+++ maps/etx_battery-compile-final.bat
@@ -1,4 +1,4 @@
-set VERSION=20111023
+set VERSION=20111025
 set MAPNAME=etx_battery
 cd ../../bin/win32
 PATH=%PATH%;%CD%
@@ -10,7 +10,7 @@ ETXMap.exe -fs_basepath %BASEPATH% -v -meta -leaktest maps/%MAPNAME%.map
 REM pause
 ETXMap.exe -fs_basepath %BASEPATH% -vis maps/%MAPNAME%.map
 REM pause
-ETXMap.exe -fs_basepath %BASEPATH% -light -fast -filter -samplesize 1 -lightmapsize 2048 -randomsamples -bounce 0 maps/%MAPNAME%.map
+ETXMap.exe -fs_basepath %BASEPATH% -light -fast -filter -samplesize 1 -lightmapsize 2048 -skyscale 2.0 -pointscale 1.3 -randomsamples -bounce 0 maps/%MAPNAME%.map
 REM ETXMap.exe -fs_basepath %BASEPATH% -light -fast -filter -samplesize 1 -lightmapsize 2048 maps/%MAPNAME%.map
 REM ETXMap.exe -fs_basepath %BASEPATH% -v -minimap -size 512 -o levelshots/%MAPNAME%_cc2.png maps/%MAPNAME%
 
